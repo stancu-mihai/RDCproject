051272a6c668350bbd039af80229bcc46d427bd7
Mihai <mihaistancu@mail.com>
2019-03-22 22:04:24 +0200

    Allows clients to send multiple messages and to exit with <EXIT> keyword

--- Client/Program.cs
+++ Client/Program.cs
@@ -34,16 +34,24 @@ namespace Client
                     Console.WriteLine("Socket connected to {0}",
                         sender.RemoteEndPoint.ToString());
 
-                    // Encode the data string into a byte array.  
-                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+                    do
+                    {
+                        var str = Console.ReadLine();
+                        // Encode the data string into a byte array.  
+                        byte[] msg = Encoding.ASCII.GetBytes(str);
 
-                    // Send the data through the socket.  
-                    int bytesSent = sender.Send(msg);
+                        // Send the data through the socket.  
+                        int bytesSent = sender.Send(msg);
 
-                    // Receive the response from the remote device.  
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                        // Receive the response from the remote device.  
+                        int bytesRec = sender.Receive(bytes);
+                        Console.WriteLine("Echoed test = {0}",
+                            Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                        if (str.Contains("<EXIT>"))
+                            break;
+
+                    } while (true);
 
                     // Release the socket.  
                     sender.Shutdown(SocketShutdown.Both);
--- Server/Program.cs
+++ Server/Program.cs
@@ -2,15 +2,12 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Server
 {
     public class SynchronousSocketListener
     {
-
-        // Incoming data from the client.  
-        public static string data = null;
 
         public static void StartListening()
         {
@@ -38,31 +35,41 @@ namespace Server
                 // Start listening for connections.  
                 while (true)
                 {
-                    Console.WriteLine("Waiting for a connection...");
+                    Console.WriteLine("Waiting for a new connection on thread " + Thread.CurrentThread.ManagedThreadId);
                     // Program is suspended while waiting for an incoming connection.  
                     Socket handler = listener.Accept();
-                    data = null;
-
-                    // An incoming connection needs to be processed.  
-                    while (true)
+                    new Thread(() =>
                     {
-                        int bytesRec = handler.Receive(bytes);
-                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                        if (data.IndexOf("<EOF>") > -1)
+                        try
                         {
-                            break;
+                            Console.WriteLine("New connection accepted on thread " + Thread.CurrentThread.ManagedThreadId);
+                            do
+                            {
+                                int bytesRec = handler.Receive(bytes);
+                                string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                                // Show the data on the console.  
+                                Console.WriteLine("Text received on thread {0}: {1}", Thread.CurrentThread.ManagedThreadId, data);
+
+                                // Echo the data back to the client.  
+                                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                                handler.Send(msg);
+
+                                if (data.IndexOf("<EXIT>") > -1)
+                                {
+                                    break;
+                                }
+
+                            } while (true);
+                            handler.Shutdown(SocketShutdown.Both);
+                            handler.Close();
                         }
-                    }
-
-                    // Show the data on the console.  
-                    Console.WriteLine("Text received : {0}", data);
-
-                    // Echo the data back to the client.  
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-
-                    handler.Send(msg);
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
+                        catch (Exception ex)
+                        {
+                            Console.Write(ex.Message);
+                        }
+                    }).Start();
                 }
 
             }
