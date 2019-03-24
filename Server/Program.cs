ff73fb14807c7516a94ac682902270e2c425e89f
Mihai <mihaistancu@mail.com>
2019-03-24 18:21:44 +0200

    Refactors the Thread worker into a separate function

--- Server/Program.cs
+++ Server/Program.cs
@@ -45,42 +45,7 @@ namespace Server
                     broadcastList.Add(handler);
                     new Thread(() =>
                     {
-                        try
-                        {
-                            Console.WriteLine("New connection accepted on thread " + Thread.CurrentThread.ManagedThreadId);
-                            do
-                            {
-                                int bytesRec = handler.Receive(bytes);
-                                string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
-
-                                // Echo the data back to the client.  
-                                byte[] msg = Encoding.ASCII.GetBytes(data);
-                              
-                                if (data.IndexOf("<EXIT>") > -1)
-                                {
-                                    Console.WriteLine("Client on thread {0} disconnected", Thread.CurrentThread.ManagedThreadId);
-                                    broadcastList.Remove(handler);
-                                    handler.Shutdown(SocketShutdown.Both);
-                                    handler.Close();
-                                    break;
-                                }
-
-                                // Show the data on the console.  
-                                Classes.Transmission t = JsonConvert.DeserializeObject<Classes.Transmission>(data);
-                                Console.WriteLine("Received message from {0} on thread {1} with the message {2} at {3}",
-                                    t.nickname, Thread.CurrentThread.ManagedThreadId, t.message, t.time);
-
-                                foreach (Socket sck in broadcastList)
-                                {
-                                    sck.Send(msg);
-                                }
-
-                            } while (true);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.Write(ex.Message);
-                        }
+                        ThreadTasks(handler, bytes);
                     }).Start();
                 }
 
@@ -93,6 +58,46 @@ namespace Server
             Console.WriteLine("\nPress ENTER to continue...");
             Console.Read();
 
+        }
+
+        public static void ThreadTasks(Socket handler, byte[]  bytes)
+        {
+            try
+            {
+                Console.WriteLine("New connection accepted on thread " + Thread.CurrentThread.ManagedThreadId);
+                do
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    if (data.IndexOf("<EXIT>") > -1)
+                    {
+                        Console.WriteLine("Client on thread {0} disconnected", Thread.CurrentThread.ManagedThreadId);
+                        broadcastList.Remove(handler);
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        break;
+                    }
+
+                    // Show the data on the console.  
+                    Classes.Transmission t = JsonConvert.DeserializeObject<Classes.Transmission>(data);
+                    Console.WriteLine("Received message from {0} on thread {1} with the message {2} at {3}",
+                        t.nickname, Thread.CurrentThread.ManagedThreadId, t.message, t.time);
+
+                    foreach (Socket sck in broadcastList)
+                    {
+                        sck.Send(msg);
+                    }
+
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
         }
 
         public static int Main(String[] args)
